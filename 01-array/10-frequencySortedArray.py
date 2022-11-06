681efce32033737378270cd1c5f1434e36b88114
salimmujawar <salim685@gmail.com>
2022-11-06 08:25:57 +0400

    Find Frequencies in a Sorted Array

---
+++ 01-array/10-frequencySortedArray.py
@@ -0,0 +1,9 @@
+#Find Frequencies in a Sorted Array
+
+#Input Array
+
+numArr = [10,10,10,25,30,30] #O/P: 10=3,25=1,30=2
+numArr = [10,10,10] #O/P: 10=4
+numArr = [10,20,30] #O/P: 10=1,20=1,30=1
+
+#Time Complexity O(n)
